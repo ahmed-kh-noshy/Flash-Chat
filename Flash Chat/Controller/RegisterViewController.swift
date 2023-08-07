956971ca47b665eee5a37c6deeae6d61af1ead1b
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-07 19:39:17 +0300

    modify register and log in view controller

--- Flash Chat/Controller/RegisterViewController.swift
+++ Flash Chat/Controller/RegisterViewController.swift
@@ -8,6 +8,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -15,6 +16,15 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email  = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error{
+                    print(error.localizedDescription)
+                }else{
+                    self.performSegue(withIdentifier: "registerToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
